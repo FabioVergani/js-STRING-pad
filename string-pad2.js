a1833f4cb29bc5ffd58e32c5cb394fffe8156f8c
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-25 09:30:22 +0200

    Update string-pad2.js

--- string-pad2.js
+++ string-pad2.js
@@ -1,4 +1,5 @@
 String.pad=function(a,b,c){
 var n=null;
-return b && a!=='' && a!=n?(a=String(a),n=a.length,Array(n?Math.max(b-n+1,0):b).join(c||'\u0020')+a):a
-}; 
+return a!=='' && a!=n && b>0?(a=String(a),n=a.length,Array(n?Math.max(b-n+1,0):b).join(c||'\u0020')+a):a
+};
+   
